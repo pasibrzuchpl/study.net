c3e8e22b047e8823327719171863a2c223b60deb
Paweł Duda <pawelduda92@gmail.com>
2016-07-30 23:13:51 +0200

    fast sorting

---
+++ FastArraySort/FastArraySort/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastArraySort
+{/*argoritm to fast sorting of array*/
+
+    class Program
+    {
+        //sorting
+        public void FastSort(double[] Array, int left, int right)
+        {
+            //vars
+            int l = left;
+            int r = right;
+
+
+            //we need center point's value  in array
+            double center = Array[(l + r) / 2];
+
+
+
+            do
+            {
+                while (Array[l] < center)
+                {
+                    l++;
+                }
+
+                while (Array[r] > center)
+                {
+                    r--;
+                }
+
+                if (l <= r)
+                {
+                    double buffor = Array[l];
+                    Array[l] = Array[r];
+
+                    l++;
+                    r--;
+
+                }
+            }
+
+            while (l <= r);
+
+            if(left<r)
+            {
+                FastSort(Array, left, r);
+            }
+            if(right>l)
+            {
+                FastSort(Array, l, right);
+            }
+        }
+
+
+        //MAIN
+        static void Main(string[] args)
+        {
+        }
+    }
+}
