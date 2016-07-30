05d511e557fd7bad27bf490de4410f63f0e17d19
Paweł Duda <pawelduda92@gmail.com>
2016-07-31 00:25:21 +0200

    FINISHED AND WORK

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose size of Array");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+
+            //our array to sorting
+            double[] Array = new double[size];
+
+            //random number
+            Random random = new Random(1000);
+
+            //array filling
+            for (int i = 0; i < size; i++)
+            {
+                Array[i] = random.Next(0, 100);
+            }
+
+            Console.WriteLine("Random array of values:");
+            foreach (double d in Array)
+            {
+                Console.Write(d + ", ");
+            }
+
+            Sort s = new Sort();
+
+            Console.WriteLine("\nSorted array");
+            s.sort(Array, size);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/Sort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Sort
+    { //bubble sorting algoritm
+        public void sort(double[] Array, int n)
+        {
+            double buffor;
+            //double loop for sorting
+            for (int i = n - 1; i > 0; i--)
+                for (int j = 0; j < i; j++)
+                {
+                    if (Array[j]> Array[j+1])
+                    {
+                        buffor = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = buffor;
+                    }
+                }
+            foreach (double d in Array)
+            {
+                Console.Write(d + ", ");
+            }
+        }
+    }
+}
