595b0a09d408edea0e467ddd859b4fb267ea2d96
Paweł Duda <pawelduda92@gmail.com>
2016-07-31 13:00:31 +0200

    first step

---
+++ PrimeNumbers/PrimeNumbers/PrimeNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class PrimeNumber
+    {
+        public void L(int N)
+        {
+            int i, j;
+            int low = 3;
+            int ans;
+
+
+            for (i = 2; i <= N;)
+            {
+                for (j = 2; j <= i - 1; j++)
+                {
+                    if (low % j == 0)
+                        break;
+                }
+                if (j == i)
+                {
+                    ans = low;
+                    i++;
+                }
+                low++;
+                
+            }
+            Console.WriteLine(ans);
+            Console.ReadKey();
+
+        } 
+
+            
+           
+        }
+    }
---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {/* PROGRAM TO FINDING PRIME NUMBER BY THEY ORDER
+        e.g. L(10)=47, because 47 is 10th prime number*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type L(N) to find:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            PrimeNumber L = new PrimeNumber();
+            L.L(n);
+
+        }
+    }
+}
