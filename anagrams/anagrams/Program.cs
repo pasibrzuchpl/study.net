2ee6455202db9311730f3fd78d262897198379a5
Paweł Duda <pawelduda92@gmail.com>
2016-08-17 21:02:59 +0200

    finished

---
+++ anagrams/anagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagrams
+{
+    /*program to checking anagrams*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+
+            Console.WriteLine("Write first word");
+            str1 = Console.ReadLine();
+
+            Console.WriteLine("Write second word, which is anagram of first");
+            str2 = Console.ReadLine();
+
+            //runing class with method 
+            anagram.anagraming(str1, str2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ anagrams/anagrams/anagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagrams
+{
+    class anagram
+    {
+        //static method od faster run in program without creating objects
+        public static void anagraming(string str1, string str2)
+        {
+            //changing to arrays of chars
+            char[] firstWord = str1.ToCharArray();
+            char[] secondWord = str2.ToCharArray();
+
+            //sorting 
+            Array.Sort(firstWord);
+            Array.Sort(secondWord);
+
+            //sorted arrays to new string 
+            string s1 = new string(firstWord);
+            string s2 = new string(secondWord);
+
+            //compering strings
+            if (s1 == s2)
+                Console.WriteLine("{0} is anagram of {1}", str1, str2);
+            else
+                Console.WriteLine("That's not anagrams");
+
+        }
+    }
+}
